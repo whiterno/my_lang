f263228bf3fd5be63a88d5719dd80be60547ef9c
whiterno <timofeikryuchkov@gmail.com>
2024-12-21 09:55:27 +0300

    parsing exp

--- frontend/include/parsing.h
+++ frontend/include/parsing.h
@@ -17,9 +17,10 @@
 #define _LINE(token)                token->line
 
 #define _CHECK_FUNC_DECL_TEMP(...)       if (! (_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == DECLARATION &&                  \
-                                               _TYPE_T(_NT) == TokenType::IDENTIFIER &&                                           \
+                                               _TYPE_T(_NT) == TokenType::IDENTIFIER &&                                            \
                                                _TYPE_T(_NNT) == TokenType::KEYWORD && _KWD_T(_NNT) == BRACKET_OP)) __VA_ARGS__;
 #define _CHECK_VAR_DECL_TEMP(...)        if (! (_KWD_T(_T) == DECLARATION && _TYPE_T(_NT) == TokenType::IDENTIFIER)) __VA_ARGS__;
+#define _CHECK_FUNCCALL(...)
 #define _CHECK_SEMICOLON(...)            if (! (_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == SEMICOLON)) __VA_ARGS__;
 #define _CHECK_OPEN_BRACKET(...)         if (!(_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == BRACKET_OP)) __VA_ARGS__;
 #define _CHECK_CLOSE_BRACKET(...)        if (!(_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == BRACKET_CL)) __VA_ARGS__;
@@ -29,8 +30,9 @@
 #define _CONST(token)               createNode(NULL, NULL, CONSTANT, (NodeValue){.number = token->value.number})
 #define _IDENT_N(token)             createNode(NULL, NULL, IDENTIFIER, (NodeValue){.index = getIndex(nt, _IDENT_T(token))})
 #define _KWD_N(kwd_type)            createNode(NULL, NULL, KEYWORD, (NodeValue){.keyword_type = kwd_type})
-#define _VAR_DECL(token)            createNode(NULL, NULL, VAR_DECLARATION, (NodeValue){.index = getIndex(nt, _IDENT_T(token))});
+#define _VAR_DECL(token)            createNode(_KWD_N(NUMBER), NULL, VAR_DECLARATION, (NodeValue){.index = getIndex(nt, _IDENT_T(token))});
 #define _FUNC_DEF(token)            createNode(_KWD_N(NUMBER), parameters_node, FUNCTION_DEFINITION, (NodeValue){.index = getIndex(nt, _IDENT_T(token))})
+#define _FUNC_CALL(token)           createNode(getArguments(tokens, nt), _IDENT_N(token), CALL, DUMMY)
 #define _SEMICOLON(statement)       createNode(statement, getProgramm(tokens, nt), KEYWORD, (NodeValue){.keyword_type = SEMICOLON})
 
 #define _PRIORITY0_FUNC(func)       *tokens = _NT;                                                              \
@@ -40,11 +42,30 @@
                                     _CHECK_CLOSE_BRACKET(syntaxError(EXPRESSION_NOT_FULL));                     \
                                     *tokens = _NT;                                                              \
                                     return ret;
+
+#define _PRIORITIES(oper, num1, num2)   if (_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == oper){           \
+                                        *tokens = _NT;                                                          \
+                                        return _##oper(priority##num1##_node, getPriority##num2(tokens, nt));   \
+                                        }
+
 #define _SIN(node)                  createNode(NULL, node, KEYWORD, (NodeValue){.keyword_type = SIN})
 #define _COS(node)                  createNode(NULL, node, KEYWORD, (NodeValue){.keyword_type = COS})
 #define _SQRT(node)                 createNode(NULL, node, KEYWORD, (NodeValue){.keyword_type = SQRT})
 #define _NOT(node)                  createNode(NULL, node, KEYWORD, (NodeValue){.keyword_type = NOT})
+#define _POW(node_l, node_r)        createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = POW})
+#define _MULT(node_l, node_r)       createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = MULT})
+#define _DIV(node_l, node_r)        createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = DIV})
+#define _ADD(node_l, node_r)        createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = ADD})
+#define _SUB(node_l, node_r)        createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = SUB})
 
+#define _EQUAL(node_l, node_r)      createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = EQUAL})
+#define _LESS(node_l, node_r)       createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = LESS})
+#define _GREATER(node_l, node_r)    createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = GREATER})
+#define _L_OR_EQ(node_l, node_r)    createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = L_OR_EQ})
+#define _G_OR_EQ(node_l, node_r)    createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = G_OR_EQ})
+#define _N_EQ(node_l, node_r)       createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = N_EQ})
+#define _AND(node_l, node_r)        createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = AND})
+#define _OR(node_l, node_r)         createNode(node_l, node_r, KEYWORD, (NodeValue){.keyword_type = OR})
 
 enum SyntaxError{
     DOUBLE_INITIALIZATION   = 1,
@@ -55,6 +76,7 @@ enum SyntaxError{
     NO_OPENING_BRACKET      = 6,
     NO_CLOSING_BRACKET      = 7,
     EXPRESSION_NOT_FULL     = 8,
+    WRONG_FUNC_CALL         = 9,
 };
 
 #endif
--- frontend/src/parsing.cpp
+++ frontend/src/parsing.cpp
@@ -12,10 +12,12 @@ static size_t scope_cnt = 0;
 static size_t func_cnt  = 0;
 
 static Node* getProgramm(Token** tokens, Nametable* nt);
+static Node* getStatement(Token** tokens, Nametable* nt);
 
 static Node* getFuncDeclaration(Token** tokens, Nametable* nt);
 static Node* getVarDeclaration(Token** tokens, Nametable* nt);
 static Node* getParameters(Token** tokens, Nametable* nt);
+static Node* getFuncCall(Token** tokens, Nametable* nt);
 
 static Node* getExpression(Token** tokens, Nametable* nt);
 static Node* getPriority5(Token** tokens, Nametable* nt);
@@ -24,7 +26,13 @@ static Node* getPriority3(Token** tokens, Nametable* nt);
 static Node* getPriority2(Token** tokens, Nametable* nt);
 static Node* getPriority1(Token** tokens, Nametable* nt);
 static Node* getPriority0(Token** tokens, Nametable* nt);
+
 static Node* getValue(Token** tokens, Nametable* nt);
+static Node* getIdentifier(Token** tokens, Nametable* nt);
+static Node* getNumber(Token** tokens, Nametable* nt);
+
+static Node* getArguments(Token** tokens, Nametable* nt);
+static Node* getArg(Token** tokens, Nametable* nt);
 
 static void syntaxError(SyntaxError err);
 static const char* errString(SyntaxError err);
@@ -40,11 +48,19 @@ Node* parsing (const char* filename){
 
 static Node* getProgramm(Token** tokens, Nametable* nt){
     if (*tokens == NULL) return NULL;
-    if (_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == BRACE_CL) return NULL;
+
+    Node* statement_node = getStatement(tokens, nt);
+
+    return _SEMICOLON(statement_node);
+}
+
+static Node* getStatement(Token** tokens, Nametable* nt){
+    if (*tokens == NULL) return NULL;
 
     Node* statement_node = NULL;
     if ((statement_node = getFuncDeclaration(tokens, nt)) != NULL)  return _SEMICOLON(statement_node);
     if ((statement_node = getVarDeclaration(tokens, nt)) != NULL)   return _SEMICOLON(statement_node);
+    if ((statement_node = getFuncCall(tokens, nt)))
 }
 
 static Node* getFuncDeclaration(Token** tokens, Nametable* nt){
@@ -62,8 +78,10 @@ static Node* getFuncDeclaration(Token** tokens, Nametable* nt){
     *tokens = _NNT;
 
     Node* params_tree       = getParameters(tokens, nt);
+    _CHECK_CLOSE_BRACKET(syntaxError(WRONG_FUNC_DECLARATION));
     *tokens = _NT;
     _CHECK_OPEN_BRACE(syntaxError(WRONG_FUNC_DECLARATION));
+    *tokens = _NT;
 
     Node* func_body         = getProgramm(tokens, nt);
     Node* parameters_node   = createNode(params_tree, func_body, PARAMETERS, DUMMY);
@@ -89,6 +107,7 @@ static Node* getParameters(Token** tokens, Nametable* nt){
     else                                        addNewName(nt, _IDENT_T(_NT), _LINE(_NT), true);
 
     Node* var_node  = _VAR_DECL(_NT)
+    _R(var_node)    = _IDENT_N(_NT);
     *tokens         = _NNT;
     _L(comma)       = getParameters(tokens, nt);
     _R(comma)       = var_node;
@@ -107,7 +126,6 @@ static Node* getVarDeclaration(Token** tokens, Nametable* nt){
 
     Node* ident_node    = _IDENT_N(_NT);
     Node* var_decl_node = _VAR_DECL(_NT)
-    _L(var_decl_node)   = _KWD_N(NUMBER);
 
     Node* var_ident     = _IDENT_N(_NT);
     *tokens             = _NNT;
@@ -124,6 +142,7 @@ static Node* getVarDeclaration(Token** tokens, Nametable* nt){
     }
 
     _CHECK_SEMICOLON(syntaxError(NO_SEMICOLON));
+    *tokens = _NT;
 
     return var_decl_node;
 }
@@ -139,21 +158,62 @@ static Node* getPriority5(Token** tokens, Nametable* nt){
 
     Node* priority4_node = getPriority4(tokens, nt);
 
+    _PRIORITIES(AND, 4, 5);
+    _PRIORITIES(OR, 4, 5);
+
+    return priority4_node;
 }
 static Node* getPriority4(Token** tokens, Nametable* nt){
     Node* priority3_node = getPriority3(tokens, nt);
+
+    _PRIORITIES(EQUAL, 3, 4);
+    _PRIORITIES(LESS, 3, 4);
+    _PRIORITIES(GREATER, 3, 4);
+    _PRIORITIES(L_OR_EQ, 3, 4);
+    _PRIORITIES(G_OR_EQ, 3, 4);
+    _PRIORITIES(N_EQ, 3, 4);
+
+    return priority3_node;
 }
 
 static Node* getPriority3(Token** tokens, Nametable* nt){
     Node* priority2_node = getPriority2(tokens, nt);
+    size_t sub_cnt = 0;
+
+    while (_TYPE_T(_T) == TokenType::KEYWORD && (_KWD_T(_T) == SUB || _KWD_T(_T) == ADD)){
+        if (_KWD_T(_T) == SUB) sub_cnt++;
+        *tokens = _NT;
+    }
+
+    if (sub_cnt % 2 == 0){
+        *tokens = _NT;
+
+        return _ADD(priority2_node, getPriority3(tokens, nt));
+    }
+    if (sub_cnt % 2 == 1){
+        *tokens = _NT;
+
+        return _SUB(priority2_node, getPriority3(tokens, nt));
+    }
+
+    return priority2_node;
 }
 
 static Node* getPriority2(Token** tokens, Nametable* nt){
     Node* priority1_node = getPriority1(tokens, nt);
+
+    _PRIORITIES(MULT, 1, 2);
+    _PRIORITIES(DIV, 1, 2);
+
+    return priority1_node;
 }
 
 static Node* getPriority1(Token** tokens, Nametable* nt){
     Node* priority0_node = getPriority0(tokens, nt);
+
+    _PRIORITIES(POW, 0, 1);
+
+    return priority0_node;
 }
 
 static Node* getPriority0(Token** tokens, Nametable* nt){
@@ -179,22 +239,93 @@ static Node* getPriority0(Token** tokens, Nametable* nt){
 static Node* getValue(Token** tokens, Nametable* nt){
     if (_TYPE_T(_T) == TokenType::KEYWORD){
         _CHECK_OPEN_BRACKET(syntaxError(EXPRESSION_NOT_FULL));
+
+        if (_TYPE_T(_NT) == TokenType::KEYWORD && _KWD_T(_NT) == SUB){
+            *tokens = _NNT;
+            Node* ret = getNumber(tokens, nt);
+            *tokens = _NT;
+
+            ret->value.number = (-1) * ret->value.number;
+
+            return ret;
+        }
+
         *tokens = _NT;
         Node* ret = getPriority5(tokens, nt);
+
         _CHECK_CLOSE_BRACKET(syntaxError(EXPRESSION_NOT_FULL));
         *tokens = _NT;
 
         return ret;
     }
     if (_TYPE_T(_T) == TokenType::CONSTANT){
-        Node* ret = _CONST(_T);
-        *tokens = _T;
-
-        return ret;
+        return getNumber(tokens, nt);
     }
     if (_TYPE_T(_T) == TokenType::IDENTIFIER){
-        if (_TYPE_T(_NT) == TokenType::KEYWORD && _KWD_T(_NT) == BRA)
+        if (_TYPE_T(_NT) == TokenType::KEYWORD && _KWD_T(_NT) == BRACE_OP){
+            return getFuncCall(tokens, nt);
+        }
+
+        return getIdentifier(tokens, nt);
     }
+}
+
+static Node* getIdentifier(Token** tokens, Nametable* nt){
+    Node* ident = _IDENT_N(_T);
+    *tokens = _NT;
+
+    return ident;
+}
+
+static Node* getNumber(Token** tokens, Nametable* nt){
+    Node* num = _CONST(_T);
+    *tokens = _NT;
+
+    return num;
+}
+
+static Node* getFuncCall(Token** tokens, Nametable* nt){
+    if (! (_TYPE_T(_T) == TokenType::IDENTIFIER && _TYPE_T(_NT) == TokenType::KEYWORD && _KWD_T(_NT) == BRACE_OP))
+
+    if (!(isNameInNametable(nt, _IDENT_T(_T)))) addNewName(nt, _IDENT_T(_T), _LINE(_T), true);
+
+    Token* ident_token  = _T;
+    *tokens             = _NT;
+    Node* func_call     = _FUNC_CALL(ident_token);
+
+    return func_call;
+}
+
+static Node* getArguments(Token** tokens, Nametable* nt){
+    if (_TYPE_T(_NT) == TokenType::KEYWORD && _KWD_T(_NT) == BRACKET_OP){
+        *tokens = _NNT;
+
+        return NULL;
+    }
+    if (_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == BRACKET_CL){
+        *tokens = _NT;
+
+        return NULL;
+    }
+
+    if (!(_TYPE_T(_T) == TokenType::KEYWORD && _KWD_T(_T) == COMMA)) syntaxError(WRONG_FUNC_CALL);
+
+    Node* comma = createNode(NULL, NULL, KEYWORD, (NodeValue){.keyword_type = COMMA});
+
+    if (!(isNameInNametable(nt, _IDENT_T(_NT)))) syntaxError(NO_IDENT_IN_NAMETABLE);
+
+    *tokens     = _NT;
+    Node* arg   = getArg(tokens, nt);
+    _R(comma)   = arg;
+    _L(comma)   = getArguments(tokens, nt);
+
+    return comma;
+}
+
+static Node* getArg(Token** tokens, Nametable* nt){
+    if (_TYPE_T(_T) == TokenType::CONSTANT) return getNumber(tokens, nt);
+
+    return getIdentifier(tokens, nt);
 }
 
 static void syntaxError(SyntaxError err){
